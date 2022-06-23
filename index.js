095bd41b7f0149438af2d5dd694f803ee4efd625
Ondicho <albert.ondicho@moringaschool.com>
2022-06-23 11:03:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-  for (i = 0; i < array.length; i++) {
-    if (array[i] + array[i + 1] === target) {
-      return true;
-    } else {
-      for (i=i+1;i<array.length;i++){
-        if (array[i] + array[i + 1] === target) {
-          return true;
-        } else {
-          return false
-        }
-      }
-    }
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
   }
+  return false;
 }
 
 /* 
